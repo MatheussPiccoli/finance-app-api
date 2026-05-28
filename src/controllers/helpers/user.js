e9e9317a62b13dd43247f7065a63d3d6f27510ba
Matheus Piccoli <matheuspdmoura@gmail.com>
2026-05-28 12:40:30 -0300

    refactor: create generic validation helper function file

--- src/controllers/helpers/user.js
+++ src/controllers/helpers/user.js
@@ -11,16 +11,9 @@ export const invalidEmailResponse = () =>
         message: 'Not a valid email. Please provide a valid one',
     })
 
-export const invalidIdResponse = () =>
-    badRequest({
-        message: 'The provided id is not valid',
-    })
-
 export const checkIfPasswordIsValid = (password) => password.length >= 6
 
 export const checkIfEmailIsValid = (email) => validator.isEmail(email)
-
-export const checkIfIdIsValid = (id) => validator.isUUID(id)
 
 export const userNotFoundResponse = () =>
     NotFound({ message: 'User not found' })
